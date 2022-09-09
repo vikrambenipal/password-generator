673d91b8b896b9bbe0e7c43a085048e9e31f673a
vikrambenipal <vikrambenipal@gmail.com>
2022-09-09 10:02:15 -0400

    can copy to clipboard

--- src/components/Copy.js
+++ src/components/Copy.js
@@ -4,11 +4,15 @@ import iconCopy from '../assets/images/icon-copy.svg'
 
 const Copy = ({ password }) => {
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(password);
+  }
+
   return (
     <Box border="2px solid black" width="90%" marginBottom="16px">
         <Box display="flex" flexDirection="row" justifyContent="space-between">
             <p style={{ paddingLeft: "5%"}}>{password}</p>
-            <img src={iconCopy} style={{alignSelf: 'center', paddingRight: "5%", cursor: "pointer"}}/>
+            <img onClick={handleCopy} src={iconCopy} alt="" style={{alignSelf: 'center', paddingRight: "5%", cursor: "pointer"}}/>
         </Box>
     </Box>
   )
